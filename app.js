b5def74beb69b50b61db8c1c54507f461f30f0b0
Zaid Bhimala <zaidm124@gmail.com>
2023-06-12 15:29:43 +0530

    db updated

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
